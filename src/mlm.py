893812621f267e086dd9bda6d42689a8ac2dc187
C.M. Downey <cmdowney88@gmail.com>
2023-02-17 11:04:23 -0800

    Replace config_dict[] notation with args. notation

--- src/mlm.py
+++ src/mlm.py
@@ -1,4 +1,4 @@
-import argparse
+pimport argparse
 import yaml
 
 import torch
@@ -13,13 +13,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--config', type=str)
 args = parser.parse_args()
 config_dict = vars(args)
-with open(config_dict['config'], 'r') as config_file:
+with open(args.config, 'r') as config_file:
     config_dict.update(yaml.load(config_file, Loader=yaml.Loader))
 
 
 # load pretrained model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained(config_dict["hf_model"], max_len=config_dict["max_len"])
-model = AutoModelForMaskedLM.from_pretrained(config_dict["hf_model"])
+tokenizer = AutoTokenizer.from_pretrained(args.hf_model, max_len=args.max_len)
+model = AutoModelForMaskedLM.from_pretrained(args.hf_model)
 
 # move model to GPU if available
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -28,47 +28,47 @@ model.to(device)
 # prepare training data
 train_dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path=config_dict["train_dataset_path"],
-    block_size=config_dict["dataset_block_size"],
+    file_path=args.train_dataset_path,
+    block_size=args.dataset_block_size,
 )
 
 # prepare validation data
 val_dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path=config_dict["val_dataset_path"],
-    block_size=config_dict["dataset_block_size"],
+    file_path=args.val_dataset_path,
+    block_size=args.dataset_block_size,
 )
 
 # initialize trainer class with training configs
 training_args = TrainingArguments(
-    seed=config_dict["seed"],
-    data_seed=config_dict["seed"],
+    seed=args.seed,
+    data_seed=args.seed,
     log_level="info",
-    num_train_epochs=config_dict["training_epochs"],
-    learning_rate=float(config_dict["learning_rate"]),
-    per_device_train_batch_size=config_dict["train_batch_size"],
-    evaluation_strategy=config_dict["eval_strategy"],
-    per_device_eval_batch_size=config_dict["eval_batch_size"],
-    eval_steps=config_dict["eval_steps"],
-    save_steps=config_dict["save_steps"],
-    save_total_limit=config_dict["saved_checkpoints_limit"],
-    output_dir=config_dict["checkpoints_directory"],
+    num_train_epochs=args.training_epochs,
+    learning_rate=float(args.learning_rate),
+    per_device_train_batch_size=args.train_batch_size,
+    evaluation_strategy=args.eval_strategy,
+    per_device_eval_batch_size=args.eval_batch_size,
+    eval_steps=args.eval_steps,
+    save_steps=args.save_steps,
+    save_total_limit=args.saved_checkpoints_limit,
+    output_dir=args.checkpoints_directory,
     overwrite_output_dir=True,
-    weight_decay=float(config_dict["weight_decay"]),
-    lr_scheduler_type=config_dict["lr_scheduler_type"],
-    warmup_ratio=float(config_dict["warmup_ratio"]),
-    warmup_steps=config_dict["warmup_steps"],
-    auto_find_batch_size=config_dict["auto_find_batch_size"],
-    group_by_length=config_dict["group_by_length"],
-    gradient_checkpointing=config_dict["gradient_checkpointing"],
-    gradient_accumulation_steps=config_dict["gradient_accumulation_steps"],
-    fp16=config_dict["fp16"],
-    fsdp=config_dict["torch_distributed_training"],
-    full_determinism=config_dict["full_determinism"]
+    weight_decay=float(args.weight_decay),
+    lr_scheduler_type=args.lr_scheduler_type,
+    warmup_ratio=float(args.warmup_ratio),
+    warmup_steps=args.warmup_steps,
+    auto_find_batch_size=args.auto_find_batch_size,
+    group_by_length=args.group_by_length,
+    gradient_checkpointing=args.gradient_checkpointing,
+    gradient_accumulation_steps=args.gradient_accumulation_steps,
+    fp16=args.fp16,
+    fsdp=args.torch_distributed_training,
+    full_determinism=args.full_determinism
 )
 
 data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=True, mlm_probability=config_dict["mlm_masking_prob"]
+    tokenizer=tokenizer, mlm=True, mlm_probability=args.mlm_masking_prob
 )
 
 trainer = Trainer(
