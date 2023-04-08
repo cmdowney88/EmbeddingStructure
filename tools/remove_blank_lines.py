d6b215e5b27ec75b330505509b7ef3775c81a036
C.M. Downey <cmdowney88@gmail.com>
2023-04-08 16:01:55 -0700

    Apply yapf

--- tools/remove_blank_lines.py
+++ tools/remove_blank_lines.py
@@ -1,24 +1,19 @@
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Create version of text file without blank lines")
-    parser.add_argument(
-        '--input_file',
-        type=str,
-        required=True
+    parser = argparse.ArgumentParser(
+        description="Create version of text file without blank lines"
     )
-    parser.add_argument(
-        '--output_file',
-        type=str,
-        required=True
-    )
+    parser.add_argument('--input_file', type=str, required=True)
+    parser.add_argument('--output_file', type=str, required=True)
     args = parser.parse_args()
 
-    file_lines = [line.strip() for line in open(args.input_file, 'r') if line.strip() != '']        
+    file_lines = [
+        line.strip()
+        for line in open(args.input_file, 'r') if line.strip() != ''
+    ]
 
     # Print merged lines to output file
     with open(args.output_file, 'w+') as fout:
         for line in file_lines:
             print(line, file=fout)
-
-    
