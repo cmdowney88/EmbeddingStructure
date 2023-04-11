1a398b4059a42d866b35c9d8bddfeb59b084a144
C.M. Downey <cmdowney88@gmail.com>
2023-04-11 14:36:26 -0700

    Add script to train SentencePiece vocab

---
+++ tools/train_sp.py
@@ -0,0 +1,30 @@
+import argparse
+import yaml
+
+import sentencepiece as spm
+
+# Read training configurations from YAML file
+parser = argparse.ArgumentParser(
+    description="Train SentencePiece tokenizer on raw vocab"
+)
+parser.add_argument('--config', type=str)
+args = parser.parse_args()
+config_dict = vars(args)
+with open(args.config, 'r') as config_file:
+    config_dict.update(yaml.load(config_file, Loader=yaml.Loader))
+
+spm.SentencePieceTrainer.Train(
+    input=args.train_dataset_path,
+    model_prefix=args.output_path,
+    vocab_size=args.vocab_size,
+    bos_id=0,
+    pad_id=1,
+    eos_id=2,
+    unk_id=3,
+    bos_piece='[CLS]',
+    pad_piece='[PAD]',
+    eos_piece='[SEP]',
+    unk_piece='[UNK]',
+    user_defined_symbols='[MASK]',
+    model_type=args.tokenizer_model
+)
