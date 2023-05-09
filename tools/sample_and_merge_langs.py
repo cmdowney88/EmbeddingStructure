b6b7e3a4eb1479a80b55f9ab6fe163035e1003ae
C.M. Downey <cmdowney88@gmail.com>
2023-05-09 10:28:26 -0700

    Add printouts and eliminate one loop

--- tools/sample_and_merge_langs.py
+++ tools/sample_and_merge_langs.py
@@ -31,6 +31,10 @@ for language in datafiles:
     lang_data[language]['length'] = lang_length
     total_length += lang_length
 
+print(f"Sampling and merging {len(languages)} languages")
+print(f"Total number of lines: {total_length}")
+print(f"Sampling alpha: {args.alpha}")
+
 orig_probs = {
     lang: (subdict['length'] / total_length)
     for lang, subdict in lang_data.items()
@@ -55,13 +59,14 @@ lambdas = {
 
 sample_probs_list = [sample_probs[lang] for lang in languages]
 
-sampled_lines = []
-sampled_langs = []
-for i in range(total_length):
-    sampled_lang = random.choices(languages, weights=sample_probs_list, k=1)[0]
-    sampled_lines.append(random.sample(lang_data[sampled_lang]['text'], k=1)[0])
-    sampled_langs.append(sampled_lang)
+if getattr(args, 'output_length', False):
+    output_length = args.output_length
+    print(f"Sampling {output_length} lines to output file")
+else:
+    output_length = total_length
 
 with open(args.combined_data_output, 'w') as fout:
-    for line in sampled_lines:
-        print(line, file=fout)
+    for i in range(output_length):
+        sampled_lang = random.choices(languages, weights=sample_probs_list, k=1)[0]
+        sampled_line = random.sample(lang_data[sampled_lang]['text'], k=1)[0]
+        print(sampled_line, file=fout)
