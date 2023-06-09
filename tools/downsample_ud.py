d8fa00613090f50e223d9271dc568af79ace996c
C.M. Downey <cmdowney88@gmail.com>
2023-06-09 09:40:20 -0700

    Add script to downsample UD file

---
+++ tools/downsample_ud.py
@@ -0,0 +1,34 @@
+import argparse
+import random
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Reduce the size of a Universal Dependencies CONNLU file"
+    )
+    parser.add_argument('--input_file', type=str, required=True)
+    parser.add_argument('--output_file', type=str, required=True)
+    parser.add_argument('--output_size', type=int, required=True)
+    args = parser.parse_args()
+
+    random.seed(1)
+
+    file_lines = [line.strip() for line in open(args.input_file, 'r')]
+
+    sentences = []
+    current_sentence = []
+    for line in file_lines:
+        if line == '' and len(current_sentence) > 0:
+            sentences.append(current_sentence)
+            current_sentence = []
+        elif line == '':
+            continue
+        else:
+            current_sentence.append(line)
+
+    random.shuffle(sentences)
+
+    with open(args.output_file, 'w+') as fout:
+        for sentence in sentences[:args.output_size]:
+            for line in sentence:
+                print(line, file=fout)
+            print('', file=fout)
